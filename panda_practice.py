5d7f94561c53afec205fa3ad75098f1015e5dd1c
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-17 12:25:54 -0600

    Add two columns to dataframe

--- panda_practice.py
+++ panda_practice.py
@@ -12,6 +12,10 @@ column = ["Mariya", "Batman", "Spongebob"]
 #print(data)
 
 # Add a name to the column
-titled_column = {"name": column}
-data = pd.DataFrame(titled_column)
+#titled_column = {"name": column}
+# Add height column in meters and weight column in kg to dataframe
+titled_columns = {"name": column,
+                 "height": [1.67, 1.9, 0.25],
+                 "weigh": [54, 100, 1]}
+data = pd.DataFrame(titled_columns)
 print(data)
