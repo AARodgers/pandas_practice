44198ce893da8f777d529aca68909d66e3f8ea2e
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-17 12:43:28 -0600

    Extract data from row

--- panda_practice.py
+++ panda_practice.py
@@ -16,6 +16,20 @@ column = ["Mariya", "Batman", "Spongebob"]
 # Add height column in meters and weight column in kg to dataframe
 titled_columns = {"name": column,
                  "height": [1.67, 1.9, 0.25],
-                 "weigh": [54, 100, 1]}
+                 "weight": [54, 100, 1]}
 data = pd.DataFrame(titled_columns)
-print(data)
+#print(data)
+
+# Select weight column
+weight_column = data["weight"]
+#print(weight_column)
+
+# Get Batman's weight
+batman_weight = data["weight"][1]
+#print(batman_weight)
+
+# Get Batman's row of info
+batman_info = data.iloc[1]
+# if you want to get the weight from the row data
+batman_info = data.iloc[1]["weight"]
+print(batman_info)
