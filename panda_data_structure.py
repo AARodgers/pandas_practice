0b44e5a911c42849c356d06fd2fd0375eaf3f2a8
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-18 16:07:07 -0600

    Add notes to panda data structures

---
+++ panda_data_structure.py
@@ -0,0 +1,8 @@
+# To create an environment with constant versions of dependancies:
+# conda create --name pandas101 python=3.9 pandas=1.2.4 numpy=1.20.1 ipykernel=5.3.4
+# probably need to update all version numbers
+
+# To activate environment:
+#conda activate pandas101
+
+
