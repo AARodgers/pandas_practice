2dbd465082398f3b31443158107dd6b24519f840
Amanda Rodgers <aarodgerswork@gmail.com>
2024-04-25 11:55:55 -0600

    Add sqlite files

--- select_DF.py
+++ select_DF.py
@@ -1,9 +1,11 @@
 # how to import an existing file
 
 import pandas as pd
+import_sqlite3
 
 # import file, because it is tab delimited you need second argument because comma delimited is the default
 data = pd.read_csv("bmi.csv", sep="\t")
+connection = sqlite3.connect("gta.db")
 print(data)
 
 # to get rid of unnecessary first column, go back to panda_practice.py file and add index=False
